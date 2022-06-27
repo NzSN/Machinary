36bb3349ec90b5d16a2541b23ae9900cf96a79ca
NzSN <aydenlin@foxmail.com>
2022-06-28 01:04:58 +0800

    Add TransCtx

--- src/Machine/Types.hs
+++ src/Machine/Types.hs
@@ -4,10 +4,12 @@ module Machine.Types
   ( Machine(..),
     Operation(..),
     OpArgs,
-    RemoteMachine(..)
+    RemoteMachine(..),
+    Interface(..)
   ) where
 
 type ConfigProc = String -> [String] -> Bool
+
 class Machine m where
   init :: m -> m
   term :: m -> m
@@ -20,9 +22,27 @@ class Operation op where
   (<-*>) :: Machine m => m -> op -> OpArgs -> m
 
 
+class Monad tr => TransCtx tr where
+  connect :: Machine m => tr m -> Maybe String
+  disconnect :: Machine m => tr m -> Maybe String
+  send :: Machine m => tr m -> a -> tr m
+  recv :: Machine m => tr m -> Maybe a
+
+
+-- Concrete Interfaces
+data TransTCP m = TransTCP { ipaddr  :: String,
+                             port    :: Integer,
+                             machine :: m
+                           } deriving (Show, Eq)
+
+instance TransCtx TransTCP where
+
+
+
+-- Concrete Machines
 type Options = [(String, [String])]
 data RemoteMachine =
-  RemoteMachine { address :: String,
+  RemoteMachine { ident :: String,
                   options :: Options
                 } deriving (Show, Eq)
 
