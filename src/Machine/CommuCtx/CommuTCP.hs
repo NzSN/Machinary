05d9feb420d609464bb84a164ad3c8b03e537a54
NzSN <aydenlin@foxmail.com>
2022-07-03 20:02:24 +0800

    Add CommuTCP.

--- src/Machine/CommuCtx/CommuTCP.hs
+++ src/Machine/CommuCtx/CommuTCP.hs
@@ -1,21 +1,29 @@
 {-# LANGUAGE ImplicitPrelude #-}
 
 module Machine.CommuCtx.CommuTCP
-  (CommuTCP (..)) where
+  ( CommuTCP (..),
+    Address (..),
+    TranslayerProto (..)
+  ) where
 
 import Machine.Types (CommuCtx(..))
 
 
--- Concrete Interfaces
-data CommuTCP a = CommuTCP {
-    ipaddr  :: (String, Int)
+data TranslayerProto = TCP | UDP deriving (Eq, Show)
+
+data Address = TranslayerAddress
+  { ipaddr :: String,
+    port   :: Integer,
+    proto  :: TranslayerProto
   } deriving (Show, Eq)
+
+newtype CommuTCP = CommuTCP { addr  :: Address } deriving (Show, Eq)
 
 instance CommuCtx CommuTCP where
   connect = undefined
   disconnect = undefined
 
-  send :: CommuTCP d -> d -> IO Bool
+  send :: CommuTCP -> d -> IO Bool
   send _ _ = undefined
 
   recv = undefined
