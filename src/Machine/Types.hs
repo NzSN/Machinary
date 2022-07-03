5c5a57a999447775e811ce134a7b1a652c6f3ae7
NzSN <aydenlin@foxmail.com>
2022-07-03 20:02:02 +0800

    Add class Serializable.

--- src/Machine/Types.hs
+++ src/Machine/Types.hs
@@ -1,9 +1,10 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module Machine.Types
   ( Machine(..),
-    Operation(..),
-    OpArgs,
-    RemoteMachine(..),
     CommuCtx(..),
+    Opt,
+    Serializable(..)
   ) where
 
 import qualified Data.String as Str
@@ -11,41 +12,39 @@ import qualified Data.ByteString as BStr
 
 type Opt = (String, [String])
 
+-- Serializable is a set of types which able to
+-- change it's structure to be able to transfered
+-- within CommuCtx.
+class Serializable s where
+  serialize :: s -> BStr.ByteString
+
+instance Serializable Str.String where
+  serialize = Str.fromString
+
+instance Serializable Int where
+  serialize = Str.fromString . show
+
+instance Serializable BStr.ByteString where
+  serialize s = s
+
+
+-- CommuCtx is an environment which hold all informations and
+-- entities that used to make communications with outside world
+-- from the systems where CommuCtx reside in.
 class CommuCtx tr where
-  connect :: tr dtype -> IO Bool
-  disconnect :: tr dtype-> IO Bool
-  send :: tr dtype -> dtype -> IO Bool
-  recv :: tr dtype -> IO dtype
+  connect :: tr -> IO Bool
+  disconnect :: tr -> IO Bool
+  send :: Serializable d => tr -> d -> IO Bool
+  recv :: Serializable d => tr -> IO d
 
-
+-- A Machine is a system that able to perform collection of
+-- operations, you are able to perform an operation via a Machine
+-- by apply an Operation instance to it.
 class Machine m where
-  init :: CommuCtx ctx => m ctx -> IO Bool
-  term :: CommuCtx ctx => m ctx -> IO Bool
+  enable :: CommuCtx ctx => m ctx -> IO Bool
+  disable :: CommuCtx ctx => m ctx -> IO Bool
   config :: CommuCtx ctx
          => m ctx
          -> Opt
          -> IO Bool
-
-type OpArgs = [(String, String)]
-class Operation op where
-  (<-*>) :: Machine m => m ctx -> op -> OpArgs -> m ctx
-
--- Concrete Machines
-type Options = [(String, [String])]
-data RemoteMachine i =
-  RemoteMachine {
-    ident :: String,
-    options :: Options,
-    interface :: CommuCtx i => i BStr.ByteString
-    }
-
-
-instance Machine RemoteMachine where
-  init :: CommuCtx i => RemoteMachine i -> IO Bool
-  init m = send (interface m) $ Str.fromString "Init"
-
-  term :: CommuCtx i => RemoteMachine i -> IO Bool
-  term m = send (interface m) $ Str.fromString "Term"
-
-  config :: CommuCtx i => RemoteMachine i -> Opt -> IO Bool
-  config m opt = send (interface m) $ Str.fromString $ show opt
+  iface :: CommuCtx ctx => m ctx -> ctx
